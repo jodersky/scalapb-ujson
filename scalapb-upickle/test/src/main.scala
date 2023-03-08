8edc6a6267f093ea457cf95cc95d73049012c142
Jakob Odersky <jakob@odersky.com>
2023-03-08 22:29:33 +0100

    cleanup

--- scalapb-upickle/test/src/main.scala
+++
@@ -1,9 +0,0 @@
-@main def main() =
-
-  val fmt = scalapb.upickle.JsonFormat(formatMapEntriesAsKeyValuePairs = true, includeDefaultValueFields = false)
-
-  val m = fmt.readJsonString[protos.Other](
-    """{"yo": "false", "x": 2, "y": 2, "other2": {"a": "ok"}, "ints": [5, 6], "data": [{"key": 2, "value": "a"}], "bar": "a" }"""
-  )
-
-  println(fmt.writeToJsonString(m))
